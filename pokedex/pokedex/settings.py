e1730df6f6ff4824432be36814db1b54b36895c0
Dvontre Coleman <dvontrec@gmail.com>
2019-04-09 09:31:46 -0400

    Update pokemon model to print out pokemon name

--- pokedex/pokedex/settings.py
+++ pokedex/pokedex/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # adds the pokemon app
+    'pokemon.apps.PokemonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pokedex/pokemon/models.py
+++ pokedex/pokemon/models.py
@@ -8,3 +8,7 @@ class Pokemon(models.Model):
     pokemon_name = models.CharField(max_length=255)
     pokemon_type = models.CharField(max_length=255)
     pokemon_number = models.CharField(max_length=3)
+
+    # defines a method to print pokemon as a string
+    def __str__(self):
+        return self.pokemon_name
