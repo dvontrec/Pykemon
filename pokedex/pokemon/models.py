1f60cb2c55e7bd7e61ce792609787a65b4884674
Dvontre Coleman <dvontrec@gmail.com>
2019-04-09 09:20:06 -0400

    Create pokemon class model

--- pokedex/pokemon/models.py
+++ pokedex/pokemon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+# pokemon class
+class Pokemon(models.Model):
+    pokemon_name = models.CharField(max_length=255)
+    pokemon_type = models.CharField(max_length=255)
+    pokemon_number = models.CharField(max_length=3)
