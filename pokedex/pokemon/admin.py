4d030e3924b89943d86d30e4112fabd8734fcdbc
Dvontre Coleman <dvontrec@gmail.com>
2019-04-09 09:42:07 -0400

    update pokemon to have number as primary key

--- pokedex/pokemon/admin.py
+++ pokedex/pokemon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pokemon
+
+
+# registers the pokemon model so it can be edited from the admin console
+admin.site.register(Pokemon)
--- pokedex/pokemon/models.py
+++ pokedex/pokemon/models.py
@@ -5,9 +5,13 @@ from django.db import models
 
 # pokemon class
 class Pokemon(models.Model):
+    # adds a meta class to make sure pokemon doesnt show up as 'Pokemons'
+    class Meta:
+        verbose_name_plural = "pokemon"
+
     pokemon_name = models.CharField(max_length=255)
     pokemon_type = models.CharField(max_length=255)
-    pokemon_number = models.CharField(max_length=3)
+    pokemon_number = models.CharField(primary_key=True, max_length=3)
 
     # defines a method to print pokemon as a string
     def __str__(self):
