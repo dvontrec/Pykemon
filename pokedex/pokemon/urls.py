649d10bda3b272631eb1d05ffd7e7e5979cf35e8
Dvontre Coleman <dvontrec@gmail.com>
2019-04-09 10:40:22 -0400

    add new pokemon form view

--- pokedex/pokemon/urls.py
+++ pokedex/pokemon/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 # Import the views from this directory
 from . import views
 
+# adds a name to the app
+app_name = 'pokemon'
 urlpatterns = [
     path('', views.index, name='index'),
     # show by pokemon number
-    path('show/<str:pokemon_number>', views.show, name='show')
+    path('show/<str:pokemon_number>/', views.show, name='show'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- pokedex/pokemon/views.py
+++ pokedex/pokemon/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render
-# import django httpresponse
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404,  render
+from django.http import Http404, JsonResponse
+from django.template import loader
 from .models import Pokemon
 # Create your views here.
 
 
 # defines an index view
 def index(request):
+    # grabs the list of pokemon
     pokemon_list = Pokemon.objects.order_by('pokemon_number')
-    output = ', '.join([p.pokemon_name for p in pokemon_list])
-    return HttpResponse(output)
+    # sets the context for what can be seen by the template
+    context = {
+        'pokemon_list': pokemon_list
+    }
+    # responds by rendering the index template passing in the context
+    return render(request, 'pokemon/index.html', context)
 
 
 # view for showing pokemon
 def show(request, pokemon_number):
-    response = "This is the pokemon %s"
-    return HttpResponse(response % pokemon_number)
+    # finds the pokemon by the number or returns a 404
+    pokemon = get_object_or_404(Pokemon, pk=pokemon_number)
+    return render(request, 'pokemon/details.html', {'pokemon': pokemon})
+
+
+def new(request):
+    return render(request, 'pokemon/new.html')
+
+
+def create(request):
+    return
